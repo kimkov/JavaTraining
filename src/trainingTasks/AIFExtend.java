6dc8e31932dbfbee71ad0e73a566f49bab9ef87d
kimkov <oleg.kim05@gmail.com>
2022-12-24 14:52:49 +0600

    add

---
+++ src/trainingTasks/AIFExtend.java
@@ -0,0 +1,37 @@
+package trainingTasks;
+
+// Один интерфейс может расширять другой
+interface ExtA {
+	void meth1();
+	void meth2();
+}
+
+// Теперь интерфейс В включает в себя методы meth1() и meth2() и добавляет meth3()
+interface ExtB extends ExtA {
+	void meth3();
+}
+
+// Этот класс должен реализовать все методы из интерфейса А и В
+class MyClass implements ExtB {
+	public void meth1() {
+		System.out.println("Реализация метода meth1()");
+	}
+	
+	public void meth2() {
+		System.out.println("Реализация метода meth2()");
+	}
+	
+	public void meth3() {
+		System.out.println("Реализация метода meth3()");
+	}
+}
+
+public class AIFExtend {
+	public static void main(String[] args) {
+		MyClass ob = new MyClass();
+		
+		ob.meth1();
+		ob.meth2();
+		ob.meth3();
+	}
+}
