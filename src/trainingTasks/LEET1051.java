868aef00bed35a5008136041610c51dae9f5a590
kimkov <oleg.kim05@gmail.com>
2023-01-05 09:18:51 +0600

    leet1051

--- src/trainingTasks/LEET1051.java
+++ src/trainingTasks/LEET1051.java
@@ -1,22 +1,27 @@
 package trainingTasks;
 
+import java.util.Arrays;
+
 class S1051 {
 	private int[] heights;
 	
 	S1051(int[] heights) {
-		int[] h = new int[heights.length];
-		this.heights = h;
+		this.heights = heights;
 	}
 	
 	int heightChecker(int[] heights) {
 		int ind = 0;
-		int[] expected = new int[] {1,1,1,2,3,4};
+		int[] newArr = new int[heights.length];
 		
 		for(int i = 0; i < heights.length; i++) {
-			for(int j = 0; j < expected.length; j++) {
-				if(heights[i] == expected[j]) {
-					ind += 1;
-				}
+			newArr[i] = heights[i];
+		}
+		
+		Arrays.sort(newArr);
+		
+		for(int i = 0; i != heights.length; i++) {
+			if(heights[i] != newArr[i]) {
+				ind += 1;
 			}
 		}
 		return ind;
@@ -25,7 +30,7 @@ class S1051 {
 
 public class LEET1051 {
 	public static void main(String[] args) {
-		int[] heights = new int[] {1,1,4,2,1,3};
+		int[] heights = new int[] {5,1,2,3,4};
 		
 		S1051 sl = new S1051(heights);
 		
