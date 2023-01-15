fd4638d980aeaeddbdc1372c660f9ae1557667e6
kimkov <oleg.kim05@gmail.com>
2023-01-15 15:55:46 +0600

    mergeSort

---
+++ src/trainingTasks/MITMergeSort1.java
@@ -0,0 +1,84 @@
+package trainingTasks;
+
+class SMergeSort {
+	private int[] nums;
+	
+	SMergeSort(int[] nums) {
+		this.setNums(nums);
+	}
+	
+	public int[] getNums() {
+		return nums;
+	}
+
+	public void setNums(int[] nums) {
+		this.nums = nums;
+	}
+	
+	void SolMergeSort(int[] nums) {
+		int inputLength = nums.length;
+		
+		if(inputLength < 2) {
+			return;
+			}
+		
+		int midIndex = inputLength / 2;
+		int[] leftHalf = new int[midIndex];
+		int[] rightHalf = new int[inputLength - midIndex];
+		
+		for(int i = 0; i < midIndex; i++) {
+			leftHalf[i] = nums[i];
+		}
+		
+		for(int i = midIndex; i < inputLength; i++) {
+			rightHalf[i - midIndex] = nums[i];
+		}
+		
+		SolMergeSort(leftHalf);
+		SolMergeSort(rightHalf);
+	}
+	
+	void Funcmerge(int[] nums, int[] leftHalf, int[] rightHalf) {
+		int leftSize = leftHalf.length;
+		int rightSize = rightHalf.length;
+		
+		int i = 0, j = 0, k = 0;
+		
+		while(i < leftSize && j < rightSize) {
+			if(leftHalf[i] <= rightHalf[j]) {
+				nums[k] = leftHalf[i];
+				i++;
+			} else {
+				nums[k] = rightHalf[j];
+				j++;
+			}
+			k++;
+		}
+		
+		while(i < leftSize) {
+			nums[k] = leftHalf[i];
+			i++;
+			k++;
+		}
+		
+		while(j < rightSize) {
+			nums[k] = rightHalf[j];
+			j++;
+			k++;
+		}
+	}
+}
+
+public class MITMergeSort1 {
+	public static void main(String[] args) {
+		int[] nums = new int[] {3, 41, 52, 26, 38, 57, 9, 49};
+		
+		SMergeSort sl = new SMergeSort(nums);
+		
+		sl.SolMergeSort(nums);
+		
+		for(int n = 0; n < nums.length; n++) {
+			System.out.print(nums[n] + " ");
+		}
+	}
+}
