df9ace26fddc4ae519ea2443a7b61ecb5d8638ab
kimkov <oleg.kim05@gmail.com>
2023-01-18 17:50:09 +0600

    heapsort

---
+++ src/trainingTasks/MITHeapSort.java
@@ -0,0 +1,25 @@
+package trainingTasks;
+
+class fHeapSort {
+	private int[] nums;
+	
+	fHeapSort(int[] nums) {
+		this.nums = nums;
+	}
+	
+	public int[] getNums() {
+		return nums;
+	}
+	
+	public void setNums(int[] nums) {
+		this.nums = nums;
+	}
+	
+	
+}
+
+public class MITHeapSort {
+	public static void main(String[] args) {
+		int[] nums = new int[] {3, 41, 52, 17, 26, 38, 57, 9, 49};
+	}
+}
