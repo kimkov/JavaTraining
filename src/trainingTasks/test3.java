d9dd6afe3566cd84e91e37df7aa00772c03c3ac3
kimkov <oleg.kim05@gmail.com>
2023-01-30 10:00:03 +0600

    test3

--- src/trainingTasks/test3.java
+++ src/trainingTasks/test3.java
@@ -2,16 +2,7 @@ package trainingTasks;
 
 public class test3 {
 	public static void main(String[] args) {
-		int x = 123;
-		int tmp = 0;
+		LinkedList<Integer> list = new LinkedList<>();
 		
-		while(x > 0) {
-			tmp = (tmp * 10) + x % 10;
-			x /= 10;
-		}
-
-		
-		System.out.println(tmp);
-		System.out.println(x);
 	}
 }
