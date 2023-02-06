222d7e2fbe0ee9287a7230abe63c081570a00c10
kimkov <oleg.kim05@gmail.com>
2023-02-06 09:07:20 +0600

    leet21

--- src/trainingTasks/LEET21.java
+++ src/trainingTasks/LEET21.java
@@ -1,37 +1,80 @@
 package trainingTasks;
 
-import java.util.Iterator;
-
 class S21 {
 	
-	LinkedList<Integer> list1;
-	LinkedList<Integer> list2;
+	private LinkedList<Integer> list1;
+	private int key;
 	
-	S21(LinkedList<Integer>list1, LinkedList<Integer> list2) {
+	S21(LinkedList<Integer> list1, int key) {
 		this.list1 = list1;
-		this.list2 = list2;
+		this.key = key;
 	}
 	
-//	LinkedList<Integer> Sol21() {
-//		for(Iterator i = l.i)
-//	}
+	public void setList1(LinkedList<Integer> list1) {
+		this.list1 = list1;
+	}
 	
+	public LinkedList<Integer> list1()  {
+		return list1;
+	}
+	
+	public void setKey(int key) {
+		this.key = key;
+	}
+	
+	public int getKey() {
+		return key;
+	}
+	
+	private class Node {
+		int val;
+		Node next;
+		
+		public Node(int v) {
+			val = v;
+			next = null;
+		}
+	}
+	
+	int length;
+	Node head;
+	
+	public S21() {
+		length = 0;
+		head = null;
+	}
+	
+	void add(int newVal) {
+		Node newNode = new Node(newVal);
+		newNode.val = newVal;
+		newNode.next = head;
+		head = newNode;
+	}
+	
+	public String toString() {
+		Node tmp = head;
+		String result = "[";
+		while(tmp != null) {
+			result += Integer.toString(tmp.val) + " ";
+			tmp = tmp.next;
+		}
+		result += "]";
+		return result;
+	}
 }
 
 public class LEET21 {
 	public static void main(String[] args) {
 		LinkedList<Integer> list1 = new LinkedList<>();
-		LinkedList<Integer> list2 = new LinkedList<>();
+		int key = 2;
 		
-		S21 sl = new S21(list1, list2);
+		S21 sl = new S21();
 		
-		list1.add(1);
-		list1.add(2);
-		list1.add(4);
+		sl.add(1);
+		sl.add(2);
+		sl.add(5);
+		sl.add(8);
 		
-		list2.add(1);
-		list2.add(3);
-		list2.add(4);
-		
+		System.out.print(sl);
 	}
 }
