ebccc4e8b397e3934dfe8f95e43597591c2bf989
Oleg Kim <97583952+kimkov@users.noreply.github.com>
2023-03-15 17:11:55 +0600

    edit MergeTwoSortedMain.java

--- src/leet/MergeTwoSortedList.java
+++ src/leet/MergeTwoSortedList.java
@@ -1,19 +1,5 @@
 package leet;
 
 public class MergeTwoSortedList {
-	public static void main(String[] args) {
-		LList list1 = new LList();
-		LList list2 = new LList();
-		
-		list1.appN(1);
-		list1.appN(3);
-		list1.appN(5);
-		
-		list2.appN(2);
-		list2.appN(4);
-		
-//		list1.mergeList(null, null);
-		list1.printL();
-		
-	}
+
 }
