72e81de6ec244013e7430697db703d4cbf54cdda
Oleg Kim <97583952+kimkov@users.noreply.github.com>
2023-04-19 08:38:25 +0600

    Create ALambdaDemo4.java

---
+++ src/trainingTasks/ALambdaDemo4.java
@@ -0,0 +1,34 @@
+package trainingTasks;
+
+// Генерация исключения в лямбда-выражении. 
+interface DoubleNumericArrayFunc {
+	double func(double[] n) throws EmptyArrayException;
+}
+
+class EmptyArrayException extends Exception {
+	EmptyArrayException() {
+		super("Array is empty");
+	}
+}
+
+public class ALambdaDemo4 {
+	public static void main(String[] args) throws EmptyArrayException {
+		double[] values = {1.0, 2.0, 3.0, 4.0};
+		// Это блочное лямбда-выражение вычисляет среднее 
+		// значение для массива элементов типа double.
+		
+		DoubleNumericArrayFunc average = (n) -> {
+			double sum = 0;
+			if(n.length == 0) {
+				throw new EmptyArrayException();
+			}
+			for(int i = 0; i < n.length; i++) {
+				sum += n[i];
+			}
+			return sum / n.length;
+		};
+		System.out.println("Average value is: " + average.func(values));
+		// Следующий код приводит к генерации исключения.
+		System.out.println("This should be exception: " + average.func(new double[0]));
+	}
+}
